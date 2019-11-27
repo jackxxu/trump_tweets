f34c6f85ff13332593a63290d13e91651cb56c34
Jack Xu <jackxxu@gmail.com>
2019-11-27 15:11:02 -0500

    refactor

--- data/run.rb
+++ data/run.rb
@@ -3,18 +3,11 @@ require 'date'
 
 Dir["./lib/*.rb"].each {|file| require file }
 
-DATA_FN_FORMAT = /^([^_]*_)?(\d{1,2})_(\d{1,2})_(\d{4})/
-
 rates = TnoteRates.new
 
 Dir.glob("**/*/*.txt")
-  .select {|f| !f.split('/').last.include?('T-Note') }
-  .map do |f|
-    file_nm = f.split('/').last  
-    m = file_nm.match(DATA_FN_FORMAT)
-    commodity = (m[1] || 'ags_settlement').gsub('_', '').downcase
-    dt = Date.new(m[4].to_i, m[3].to_i, m[2].to_i)
-    "#{commodity}-#{dt.to_s}"
-  end
+  .map {|path| DataFile.new(path) }
+  .filter {|f| !f.tnote? }
+  .map {|f| "#{f.commodity || 'ags_settlement'}-#{f.dt.to_s}" }
   .sort
   .each {|x| puts x}
