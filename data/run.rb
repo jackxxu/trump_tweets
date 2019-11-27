665c22896785d0bf5d6d3b886ce3aa3d8ec0b5ee
Jack Xu <jackxxu@gmail.com>
2019-11-27 14:59:40 -0500

    add run.rb

--- data/run.rb
+++ data/run.rb
@@ -1,6 +1,20 @@
 require 'pry'
+require 'date'
 
 Dir["./lib/*.rb"].each {|file| require file }
 
+DATA_FN_FORMAT = /^([^_]*_)?(\d{1,2})_(\d{1,2})_(\d{4})/
+
 rates = TnoteRates.new
 
+Dir.glob("**/*/*.txt")
+  .select {|f| !f.split('/').last.include?('T-Note') }
+  .map do |f|
+    file_nm = f.split('/').last  
+    m = file_nm.match(DATA_FN_FORMAT)
+    commodity = (m[1] || 'ags_settlement').gsub('_', '').downcase
+    dt = Date.new(m[4].to_i, m[3].to_i, m[2].to_i)
+    "#{commodity}-#{dt.to_s}"
+  end
+  .sort
+  .each {|x| puts x}
