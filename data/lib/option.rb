904247aa8d1aefadfbac079e57f809592dafd9d0
Jack Xu <jackxxu@gmail.com>
2019-12-01 05:28:10 -0500

    filter out 30% of zero volatility lines

--- data/lib/option.rb
+++ data/lib/option.rb
@@ -5,6 +5,7 @@ module CMEGroup
 
     TYPES = [
       { name: 'cso', type: :cso, future_name: '' }, # ignore all the CSOs (Calendar Spread)
+      { name: 'calendar spread option', type: :cso, future_name: '' }, # ignore all the CSOs (Calendar Spread)
       { name: 'soy bean oil', type: :soybean_oil, future_name: 'BO Soybean Oil Futures' },
       { name: 'soybean oil', type: :soybean_oil, future_name: 'BO Soybean Oil Futures' },
       { name: 'soybean meal', type: :soybean_meal, future_name: 'SM Soybean Meal Futures' },
@@ -15,11 +16,14 @@ module CMEGroup
       { name: 'soybean', type: :soybeans, future_name: 'S Soybean Futures' },
       { name: 'corn', type: :corn, future_name: 'C Corn Futures' },
       { name: 'eu milling wheat', type: :eu_miilling_wheat, future_name: 'WEU EU Milling Wheat Futures' },
+      { name: 'hard red spring wheat', type: :hard_red_wheat, future_name: 'MWE Minneapolis Hard Red Spring Wheat Futures' },
       { name: 'wheat', type: :wheat, future_name: 'W Wheat Futures' },
       { name: 'wh ', type: :wheat, future_name: 'W Wheat Futures' },
       { name: 'cheese', type: :cheese, future_name: 'CSC Cash Settled Cheese Future' },
       { name: 'dry milk', type: :dry_milk, future_name: 'NF CME NON-FAT DRY MILK FUTURES' },
       { name: 'mid-size milk', type: :mid_size_milk, future_name: 'ZX Mid-size Milk Futures' },
+      { name: 'class iv milk', type: :milk_iv, future_name: 'DK CME CLASS IV MILK FUTURES' },
+      { name: 'non-fat dry milk', type: :nonfat_dry_milk, future_name: 'NF CME NON-FAT DRY MILK FUTURES' },
       { name: 'milk', type: :milk, future_name: 'DA MILK FUTURES' },
       { name: 'feeder cattle', type: :feeder_cattle, future_name: 'FC CME FEEDER CATTLE FUTURES'},
       { name: 'palm oil', type: :palm_oil, future_name: 'FCP BMD Crude Palm Oil Future'},
